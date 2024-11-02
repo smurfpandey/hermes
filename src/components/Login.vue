838f95692e20a650716ef9c718459b01bd7b62b6
Neeraj Verma <github@smurfpandey.me>
2024-11-02 11:17:14 +0530

    added login functionality

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { supabase } from '@/lib/supabaseClient';
 import { Button } from '@/components/ui/button';
+import { Loader2 } from 'lucide-vue-next';
 import {
   Card,
   CardContent,
@@ -11,9 +15,28 @@ import {
 import { Container } from '@/components/ui/container';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useRouter } from 'vue-router';
 
 const router = useRouter();
+const email = ref('');
+const password = ref('');
+const errorMessage = ref('');
+const isLoading = ref(false);
+
+const handleLogin = async () => {
+  isLoading.value = true;
+  errorMessage.value = '';
+  const resp = await supabase.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  });
+  if (resp.error) {
+    errorMessage.value = resp.error.message;
+  } else {
+    router.push('/');
+  }
+  isLoading.value = false;
+};
+
 const goToSignup = () => {
   router.push('/signup');
 };
@@ -25,23 +48,32 @@ const goToForgotPassword = () => {
   <Container class="flex h-screen p-4 mt-[-65px]">
     <Card class="w-96 m-auto max-w-md">
       <CardHeader class="space-y-1">
-        <CardTitle class="text-2xl"> Create an account </CardTitle>
+        <CardTitle class="text-2xl"> Login </CardTitle>
         <CardDescription>
-          Enter your email below to create your account
+          Enter your email and password to login
         </CardDescription>
       </CardHeader>
       <CardContent class="grid gap-4">
         <div class="grid gap-2">
           <Label for="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
+          <Input
+            id="email"
+            type="email"
+            placeholder="m@example.com"
+            v-model="email"
+          />
         </div>
         <div class="grid gap-2">
           <Label for="password">Password</Label>
-          <Input id="password" type="password" />
+          <Input id="password" type="password" v-model="password" />
         </div>
+        <span v-if="errorMessage" class="text-red-500">{{ errorMessage }}</span>
       </CardContent>
       <CardFooter>
-        <Button class="w-full"> Create account </Button>
+        <Button class="w-full" @click="handleLogin" :disabled="isLoading">
+          <Loader2 class="w-4 h-4 mr-2 animate-spin" v-if="isLoading" />
+          Login
+        </Button>
         <Button class="w-full" variant="link" @click="goToForgotPassword">
           Forgot Password?
         </Button>
